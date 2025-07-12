7933fe9161bb6ee701183539c6ce9033a070f2fe
Kolahalan <kolahalan91@gmail.com>
2025-07-12 14:31:37 +0530

    using localstorage for stting and retriving data from Service

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -37,6 +37,14 @@ export class TasksService {
   },
 ]
 
+constructor(){
+    const tasks = localStorage.getItem('tasks')
+    if(tasks)
+    {
+        this.tasks = JSON.parse(tasks)
+    }
+}
+
 getUserTasks(userId:string) {
     return this.tasks.filter((task) => task.userId === userId)
 }
@@ -50,10 +58,15 @@ addTask(taskData:NewTaskData,userId:string){
     dueDate: taskData.date
   };
     this.tasks = [newTask, ...this.tasks]; // Add at the beginning
+    this.saveTasks()
 }
 
 removeTasks (id:string){
        this.tasks =  this.tasks.filter((task) => task.id !== id) ;
+    this.saveTasks()
+    }
+private saveTasks() {
+    localStorage.setItem('tasks',JSON.stringify(this.tasks))
 }
 
 }
